a10454c8f2fdc3e5c77214b9c9f016a6c2bd1ca1
zachsnoek <zacharysnoek@hope.edu>
2018-11-13 11:07:38 -0500

    Clean up and change file names

--- src/CSVParser.java
+++ src/CSVParser.java
@@ -7,82 +7,23 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
-public class Parser {
-	static HashMap<String, String> csvFiles = new HashMap<String, String>();
-	
-	public static void main(String[] args) throws FileNotFoundException {
-		csvFiles.put("sp19", "19SP");
-		csvFiles.put("fa18", "18FA");
-		csvFiles.put("sp18", "18SP");
-		csvFiles.put("fa17", "17FA");
-		csvFiles.put("sp17", "17SP");
-		csvFiles.put("fa16", "16FA");
-		csvFiles.put("sp16", "16SP");
-		csvFiles.put("fa15", "15FA");
-		csvFiles.put("sp15", "15SP");
-		csvFiles.put("fa14", "14FA");
-		csvFiles.put("sp14", "14SP");
-		csvFiles.put("fa13", "13FA");
-		csvFiles.put("sp13", "13SP");
-		csvFiles.put("fa12", "12FA");
-		csvFiles.put("sp12", "12SP");
-		csvFiles.put("fa11", "11FA");
-		csvFiles.put("sp11", "11SP");
-		csvFiles.put("fa10", "10FA");
-		csvFiles.put("sp10", "10SP");
-		Parser p = new Parser();
-	}
+public class CSVParser {
+	// The working directory for CSV files
+    String wd;
     
-	public Parser() throws FileNotFoundException {
-        PrintWriter pw = new PrintWriter(new File("main.csv"));
-        StringBuilder sb = new StringBuilder();
-        sb.append("Year");
-        sb.append(',');
-        sb.append("Title");
-        sb.append(',');
-        sb.append("Instructor");
-        sb.append(',');
-        sb.append("Credits");
-        sb.append(',');
-        sb.append("Actual");
-        sb.append(',');
-        sb.append("Capacity");
-        sb.append('\n');
-        pw.write(sb.toString());
-        
-		for (String f : csvFiles.keySet()) {
-			ArrayList<Class> c = parse(f, csvFiles.get(f));
-			sb = new StringBuilder();
-			for (Class cl : c) {
-				sb.append(cl.getYear());
-				sb.append(',');			
-				sb.append(cl.getTitle());
-				sb.append(',');
-				sb.append(cl.getInstructor());
-				sb.append(',');
-				sb.append(cl.getCredits());
-				sb.append(',');
-				sb.append(cl.getActual());
-				sb.append(',');
-				sb.append(cl.getCapacity());
-				sb.append('\n');
-			}
-			pw.write(sb.toString());
-		}
-        
-		pw.close();
-        System.out.println("Done!");
+	public CSVParser(String wd) throws FileNotFoundException {
+		this.wd = wd;
     }
     
 	public ArrayList<Class> parse(String file, String year) {
 		ArrayList<Class> classes = new ArrayList<Class>();
 		
-		String workingDir = "/Users/zacharysnoek/Programming/java/course-schedule-parser/csv/";
+		String workingDir = wd;
 	    String l = "";
 	    
-	    String fa18 = workingDir + file + ".csv";
+	    String csvFile = workingDir + file + ".csv";
 	    
-	    try (BufferedReader br = new BufferedReader(new FileReader(fa18))) {
+	    try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
 	
 	    		while ((l = br.readLine()) != null) {
 	            ArrayList<String> line = parseLine(l);
@@ -112,6 +53,9 @@ public class Parser {
 	    return classes;
 	}
 	
+	/*
+	 * Use this function to properly handle commas in course titles and names
+	 */
     public ArrayList<String> parseLine(String line) {
 		char[] input = line.toCharArray();
 		StringBuilder sb = new StringBuilder();
